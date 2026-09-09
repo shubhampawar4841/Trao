65dd0c1e469c512724f3062c6b0ded479428ade4
shubhampawar4841 <shubhampawar4036@gmail.com>
2026-09-09 21:21:49 +0530

    Update CORS configuration to allow requests from the new frontend URL
    - Changed the allowed origin in the CORS middleware from "https://trao-two.vercel.app" to "https://trao-frontend-tau.vercel.app" to reflect the updated production environment.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:3000",
-    origins: ["https://trao-two.vercel.app"],
+    origins: ["https://trao-frontend-tau.vercel.app"],
     credentials: true,
   })
 );
